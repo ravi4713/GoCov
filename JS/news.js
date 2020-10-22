f6fe913996785dc1454f780462f36ca7f69c33a7
ravi rungta <4713rkr@gmail.com>
2020-10-22 12:46:27 +0530

    News api added

---
+++ JS/news.js
@@ -0,0 +1,50 @@
+const request =require('postman-request')
+const api ='9cbbba7d25834dad9ec9c3b48e6d4337'
+
+
+
+const newReq = (callback) => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey='+api+'&category=health&q=corona'
+    request({url: url,json: true},(error,response) => {
+        if (error){
+            callback('Check Your Network Connention! failed to Connent',undefined)
+        } else {
+            callback(undefined,response)
+        }
+    })
+}
+
+newReq((error,data) =>{
+    if (error){
+        console.log(error)
+    } else {
+        let html = ``
+     const articles = data.body.articles
+     console.log(articles[0].source)
+     articles.forEach((element,index) => {
+        var source = element.source.name
+        var title = element.title
+        var description = element.description
+        var url = element.url
+        html += `<div class="card">
+        <div class="card-header" id="heading-${index}">
+          <h2 class="mb-0">
+            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse-${index}" aria-expanded="false" aria-controls="collapse-${index}">
+              ${title}
+            </button>
+          </h2>
+        </div>
+    
+        <div id="collapse-${index}" class="collapse show" aria-labelledby="heading-${index}" data-parent="#accordionExample">
+          <div class="card-body">
+            <h3>Source : ${source}</h3>
+            ${description}
+            <a href='${url}'>About More</a>
+          </div>
+        </div>
+      </div>`
+     });
+    document.getElementById('accordionExample').innerHTML = html;
+    }
+
+})
